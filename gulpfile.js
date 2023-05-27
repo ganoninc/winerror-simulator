f7212714d0a3cbdb15fc39e28c5bb17f6ca9f96b
Romain Giovanetti <ganoninc@gmail.com>
2023-05-27 15:21:04 +1000

    upgrade gulp from 3.9.1 to 4.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,70 @@
-'use strict';
-
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const { watch, parallel, series, src, dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const webserver = require('gulp-webserver');
 const uglify = require('gulp-uglify');
 const gulpCopy = require('gulp-copy');
 const cleanCSS = require('gulp-clean-css');
-const image = require('gulp-image');
+// TODO fix image() import / call 
+// const image = require('gulp-image');
 const rjs = require('gulp-requirejs');
 
-gulp.task('js:buildRJSAndcompress', function() {
-    return rjs({
-        name: 'app',
-        baseUrl: './js/',
-        out: 'app.js',
-    })
+
+function jsBuildRJSAndcompress() {
+  return rjs({
+    name: "app",
+    baseUrl: "./js/",
+    out: "app.js",
+  })
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js/')); // pipe it to the output DIR 
-});
+    .pipe(dest("./build/js/"));
+}
 
-gulp.task('img:optimize', function () {
-  gulp.src('./img/*')
-  .pipe(image())
-  .pipe(gulp.dest('./build/img'));
-});
+function imgOptimize() {
+  // TODO fix image() import / call 
+  // return src("./img/*").pipe(image()).pipe(dest("./build/img"));
+  return src("./img/*").pipe(dest("./build/img"));
+}
 
-gulp.task('css:compress', function () {
-    gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./build/css'));
-});
+function cssCompress() {
+  return src("./sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS({}))
+    .pipe(dest("./build/css"));
+}
 
-gulp.task('index:copy', function () {
-  gulp.src('./index.html')
-  .pipe(gulpCopy('./build'));
-});
+function indexCopy() {
+  return src("./index.html").pipe(gulpCopy("./build"));
+}
 
-gulp.task('build', ['css:compress', 'js:buildRJSAndcompress', 'img:optimize', 'index:copy']);
+exports.build = series(
+  cssCompress,
+  jsBuildRJSAndcompress,
+  imgOptimize,
+  indexCopy
+);
 
-gulp.task('sass:build', function () {
-    gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+function sassBuild() {
+  return src("./sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./css"));
+}
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/*.sass', ['sass:build']);
-});
+function sassWatch() {
+  return watch("./sass/*.sass", sassBuild);
+}
 
-gulp.task('webserver', function() {
-  gulp.src('./')
-  .pipe(webserver({
-      'host': '0.0.0.0'
-  }));
-});
+function webServer() {
+  return src("./").pipe(
+    webserver({
+      host: "localhost",
+    })
+  );
+}
 
-gulp.task('default', ['sass:build', 'sass:watch', 'webserver']);
+exports.dev = series(
+  sassBuild,
+  parallel(
+    sassWatch,
+    webServer
+  )
+);
